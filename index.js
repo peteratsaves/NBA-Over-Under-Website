3bb8e723ce707143defa3da4a7b992c82c427d90
Peter Atsaves <atsavespeter21@gmail.com>
2023-11-22 17:58:22 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict;"
+
+teams = ['Atlanta Hawks','Boston Celtics','Brooklyn Nets','Charlotte Hornets','Chicago Bulls','Cleveland Cavaliers','Dallas Mavericks','Detroit Pistons','Denver Nuggets','Golden State Warriors','Houston Rockets','Indiana Pacers','Los Angeles Clippers','Los Angeles Lakers','Memphis Grizzlies','Miami Heat','Minnesota Timberwolves','Milwaukee Bucks','New Orleans Pelicans','New York Knicks','Oklahoma City Thunder','Orlando Magic','Phoenix Suns','Philadelphia 76ers','Portland Trail Blazers','Sacramento Kings','San Antonio Spurs','Toronto Raptors','Utah Jazz','Washington Wizards'];
+
+overs = [42.5, 54.5,37.5,31.5,37.5,50.5,43.5,28.5,52.5,47.5,31.5,39.5,45.5,46.5,46.5,44.5,44.5,53.5,4.5,45.5,44.5,37.5,51.5,47.5,27.5,44.5,28.5,36.5,35.5,24.5];
+
+people = ['Josh','Alan','Emil','Alan','Eric','Emil','Peter','Peter','Josh','Emil','Emil','Josh','Peter','Emil','Peter','Alan','Eric','Eric','Alan','Emil','Josh','Eric','Josh','Peter','Eric','Eric','Peter','Alan','Josh','Alan'];
+
+
+const myElement = document.getElementById("content");
+myElement.innerHTML = "g";
